1473a26f9b9f97e265d61b2dea28cf6820c13cd3
Raunak Shakya <shakya_raunak@yahoo.com>
2015-04-20 09:09:03 +0545

    User model class added

---
+++ app/models/User.scala
@@ -0,0 +1,37 @@
+package models
+
+import anorm._
+import anorm.SqlParser._
+
+case class User(
+                 firstName: String,
+                 lastName: String,
+                 email: String,
+                 phone: String,
+                 company: String,
+                 username: String,
+                 password: String
+                 )
+
+case class SignInForm(email: String, password: String)
+
+case class UpdateForm(id: Int, firstName: String, lastName: String, email: String, phone: String, company: String)
+
+object User {
+
+  /**
+   * Parse a User from a ResultSet
+   */
+  val simple = {
+    get[String]("user.firstName") ~
+      get[String]("user.lastName") ~
+      get[String]("user.email") ~
+      get[String]("user.phone") ~
+      get[String]("user.company") ~
+      get[String]("user.username") ~
+      get[String]("user.password") map {
+      case firstName ~ lastName ~ email ~ phone ~ company ~ username ~ password => User(firstName, lastName, email, phone, company, username, password)
+    }
+  }
+
+}
